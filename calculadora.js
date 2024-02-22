35ca5e52bfe6fc4fa8e387388a17c027c18387d4
SAH <143020123+BRINAHH@users.noreply.github.com>
2024-02-22 12:18:25 -0300

    Update calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+const calcular = () => {
+    let numero1 = document.getElementById("numero1");
+    let numero2 = document.getElementById("numero2");
+
+    resultado.value = parseInt(numero.value) + parseInt(numero2.value)
+}
